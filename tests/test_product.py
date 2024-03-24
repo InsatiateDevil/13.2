b299a13f4f4a03780c3e91a9de48b0d6b97df0e1
ILIDANUM <ILIDANUM@yandex.ru>
2024-03-24 20:07:26 +0300

    test done

--- tests/test_product.py
+++ tests/test_product.py
@@ -16,6 +16,9 @@ def test_init_new_product(null, dict_with_product):
     assert Product.products_list[0].quantity == 14
     Product.init_new_product(dict_with_product)
     assert Product.products_list[0].quantity == 28
+    dict_with_product['price'] = 35000.0
+    Product.init_new_product(dict_with_product)
+    assert Product.products_list[0].quantity == 42
 
 
 def test_price_getter(for_product):
@@ -27,3 +30,25 @@ def test_price_setter(for_product):
     assert for_product.price == 31000.0
     for_product.price = 30000.0  # При вводе 'y'
     assert for_product.price == 30000.0
+    for_product.price = 0.0
+    assert for_product.price == 30000.0
+    for_product.price = 32000.0
+    assert for_product.price == 32000.0
+
+
+def test_add(null, for_category):
+    assert Product.products_list[0] + Product.products_list[1] == 2580000.0
+
+
+def test_len(for_product):
+    assert len(for_product) == 14
+
+
+def test_str(for_product):
+    str_product = "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+    assert str(for_product) == str_product
+
+
+def test_repr(for_product):
+    repr_product = "Product(Xiaomi Redmi Note 11, 1024GB, Синий, 14)"
+    assert repr(for_product) == repr_product
